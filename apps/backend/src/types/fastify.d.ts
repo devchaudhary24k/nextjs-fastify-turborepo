cf98755c2f9a3bf4f0441982f0d0a2ae83f607a8
Dev Talan <chaudhary.dev.talan@gmail.com>
2025-04-27 05:52:56 +0000

    Fixed fastify types in backend

--- apps/backend/src/types/fastify.d.ts
+++ apps/backend/src/types/fastify.d.ts
@@ -1,8 +1,3 @@
 import "fastify";
 
-declare module "fastify" {
-  interface FastifyRequest {
-    startTime?: [number, number]; // [seconds, nanoseconds] from process.hrtime()
-    routerPath?: string; // optional route path (fallback to raw url)
-  }
-}
+declare module "fastify" {}
